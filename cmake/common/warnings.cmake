a65f7789ad203537fa2689c0547fb4e3765b589e
ermacv <ve@moeco.io>
2022-05-13 17:32:30 +0300

    Updated CMake files

--- cmake/common/warnings.cmake
+++ cmake/common/warnings.cmake
@@ -2,8 +2,8 @@
 # Add common warnings
 ################################################################################
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
-    add_compile_options(-Wall)
-    add_compile_options(-Wextra)
+    # add_compile_options(-Wall)
+    # add_compile_options(-Wextra)
     # add_compile_options(-Werror)
 endif(CMAKE_C_COMPILER_ID STREQUAL "GNU")
 ################################################################################
---
+++ modules/application/main.c
@@ -0,0 +1,7 @@
+#include "FreeRTOS.h"
+#include "task.h"
+
+int main(void) {
+  vTaskStartScheduler();
+  return 0;
+};
